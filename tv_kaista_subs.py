96f008169b0a8868e23918576142f840d5070713
Juhamatti Niemelä <iiska@isengard.(none)>
2010-01-14 16:48:39 +0200

    Corrected a few pylint warnings. Pylint grade 9.07/10 now.

--- tv_kaista_subs.py
+++ tv_kaista_subs.py
@@ -16,37 +16,20 @@ GCONF_AUTH_KEY = "/apps/gnome-python-desktop/keyring_auth_token"
 # Use this if Miro settings can't be found
 FALLBACK_VIDEO_DIR = "~/Videos"
 
-yle_filter = re.compile(".*YLE.*(srt){0}$")
-id_filter = re.compile("_(\d+)\.[A-Za-z0-9]+\.[A-Za-z0-9]+$")
+YLE_FILTER = re.compile(".*YLE.*(srt){0}$")
+ID_FILTER = re.compile("_(\d+)\.[A-Za-z0-9]+\.[A-Za-z0-9]+$")
 
 def get_mirodir():
     """ Finds Miro gconf settings and retries download directory """
     client = gconf.client_get_default()
-    s = client.get_string("/apps/miro/MoviesDirectory")
-    if s:
-        return s
+    mdir = client.get_string("/apps/miro/MoviesDirectory")
+    if mdir:
+        return mdir
     else:
         return FALLBACK_VIDEO_DIR
 
-def get_login_password():
-    """ Retrieve credentials from Gnome Keyring """
-    # From python-gnomekeyring examples
-    keyring = gnomekeyring.get_default_keyring_sync()
-    auth_token = gconf.client_get_default().get_int(GCONF_AUTH_KEY)
-
-    if auth_token > 0:
-        try:
-            secret = gnomekeyring.item_get_info_sync(keyring, auth_token).get_secret()
-        except gnomekeyring.DeniedError:
-            login = None
-            password = None
-            auth_token = 0
-        else:
-            login, password = secret.split('\n')
-    else:
-        login = None
-        password = None
-
+def password_dialog(login = None, password = None):
+    """ Simple password prompt dialog. """
     dialog = gtk.Dialog("Login to TV-kaista", None, 0,
                         (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
                          gtk.STOCK_OK, gtk.RESPONSE_OK))
@@ -96,29 +79,56 @@ def get_login_password():
             password = local_entry2.get_text()
             if not login or not password:
                 continue
-            auth_token = gnomekeyring.item_create_sync(
-                keyring,
-                gnomekeyring.ITEM_GENERIC_SECRET,
-                "TV-kaista subtitles downloader",
-                dict(appname="tv-kaista-subs-py, subtitles downloader"),
-                "\n".join((login, password)), True)
-            gconf.client_get_default().set_int(GCONF_AUTH_KEY, auth_token)
             dialog.destroy()
             return login, password
         else:
             raise SystemExit
 
-def listfiles(p, file_filter = None):
+def get_login_password():
+    """ Retrieve credentials from Gnome Keyring """
+    # From python-gnomekeyring examples
+    keyring = gnomekeyring.get_default_keyring_sync()
+    auth_token = gconf.client_get_default().get_int(GCONF_AUTH_KEY)
+
+    if auth_token > 0:
+        try:
+            secret = gnomekeyring.item_get_info_sync(keyring,
+                                                     auth_token).get_secret()
+        except gnomekeyring.DeniedError:
+            login = None
+            password = None
+            auth_token = 0
+        else:
+            login, password = secret.split('\n')
+    else:
+        login = None
+        password = None
+
+    login, password = password_dialog(login, password)
+
+    # Save new login and password to gnome keyring
+    auth_token = gnomekeyring.item_create_sync(
+        keyring,
+        gnomekeyring.ITEM_GENERIC_SECRET,
+        "TV-kaista subtitles downloader",
+        dict(appname="tv-kaista-subs-py, subtitles downloader"),
+        "\n".join((login, password)), True)
+    gconf.client_get_default().set_int(GCONF_AUTH_KEY, auth_token)
+
+    return login, password
+    
+
+def listfiles(path, file_filter = None):
     """ Traverse directory recursively and find normal files.
     If file_filter is specifed as a regex then return only files
     which match
 
-    p: path to list
+    path: path to list
     file_filter: regex to filter only wanted files
     """
     ret = []
-    for f in os.listdir(p):
-        abs_path = os.path.join(p,f);
+    for item in os.listdir(path):
+        abs_path = os.path.join(path, item)
         if os.path.isdir(abs_path):
             ret.extend(listfiles(abs_path, file_filter))
         elif os.path.isfile(abs_path) and (
@@ -128,11 +138,14 @@ def listfiles(p, file_filter = None):
     return ret
 
 def download_srt(videofile, credentials):
+    """ Download srt file from tvkaista.fi for videofile. Use credentials
+    for authentication. """
     # http://alpha.tvkaista.fi/recordings/download/[:recording_id].srt
-    f = urllib.urlopen("http://%s:%s@alpha.tvkaista.fi/recordings/download/%s.srt" %
+    f = urllib.urlopen(
+        "http://%s:%s@alpha.tvkaista.fi/recordings/download/%s.srt" %
                        (credentials[0],
                         credentials[1],
-                        id_filter.search(videofile).group(1)))
+                        ID_FILTER.search(videofile).group(1)))
     if f.getcode() == 200:
         output = open("%s%s" % (os.path.splitext(videofile)[0], ".srt"), "w")
         output.write(f.read())
@@ -144,7 +157,7 @@ def download_srt(videofile, credentials):
 
 if __name__ == "__main__":
     cred = get_login_password()
-    for f in listfiles(get_mirodir(), yle_filter):
-        download_srt(f, cred)
+    for video in listfiles(get_mirodir(), YLE_FILTER):
+        download_srt(video, cred)
 
     
