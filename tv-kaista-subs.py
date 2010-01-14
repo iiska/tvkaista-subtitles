5272b2406c1c7b7bcf990c0dd98af23385f2e88a
Juhamatti Niemelä <iiska@isengard.(none)>
2010-01-14 14:54:35 +0200

    Filter file listing according to provided regular expression.

--- tv-kaista-subs.py
+++ tv-kaista-subs.py
@@ -1,25 +1,28 @@
 #! /usr/bin/env python
 
 import os
+import re
 
 MIRO_DL_DIR = "/home/iiska/Videos/tv-kaista"
 
-def listfiles(p, filt = None):
-""" Traverse directory recursively and find normal files.
-    If filt is specifed as a regex then return only files
+def listfiles(p, file_filter = None):
+    """ Traverse directory recursively and find normal files.
+    If file_filter is specifed as a regex then return only files
     which match
 
     p: path to list
-    filter: regex to filter only wanted files
-"""
+    file_filter: regex to filter only wanted files
+    """
     ret = []
     for f in os.listdir(p):
         abs_path = os.path.join(p,f);
         if os.path.isdir(abs_path):
-            ret.append(listfiles(abs_path, fil))
-        else if os.path.isfile(abs_path):
+            ret.extend(listfiles(abs_path, file_filter))
+        elif os.path.isfile(abs_path) and (
+            (file_filter == None) or (file_filter.match(abs_path))):
             ret.append(abs_path)
 
     return ret
- 
-print listfiles(MIRO_DL_DIR)
+
+yle_filter = re.compile(".*YLE.*")
+print listfiles(MIRO_DL_DIR, yle_filter)
